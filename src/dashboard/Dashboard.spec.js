faf5af4a04cc84d173e4141cfe58c94476b22501
Afia-Lydia <you@example.com>
2019-11-11 15:59:50 -0500

    implemented tests

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,24 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import Dashboard from "./Dashboard";
 // Test away
+
+test("Dashboard renders correctly", () => {
+	render(<Dashboard />);
+});
+
+test("Gate defaults to unlocked and open", () => {
+	const { getByText, queryByText } = render(<Dashboard />);
+	getByText(/open/i);
+    expect(queryByText(/open gate/i)).toBeNull();
+    getByText(/unlocked/i)
+    expect(queryByText(/unlock gate/i)).toBeNull()
+});
+
+test("renders controls and display", () => {
+	const { getByText, getAllByText } = render(<Dashboard />);
+	getByText(/locked/); //controls
+	getAllByText(/gate/i); //display
+});
+
+// - shows the controls and display
